bac94d3a6cb3b3979a01284a77abd77ae8fa25b6
Caue Ferreira <eldender@gmail.com>
2019-03-01 02:57:12 -0300

    Small naming refactor

--- cacheapi/src/main/java/com/caueferreira/cacheapi/CacheInterface.kt
+++ cacheapi/src/main/java/com/caueferreira/cacheapi/CacheInterface.kt
@@ -2,11 +2,11 @@ package com.caueferreira.cacheapi
 
 import io.reactivex.Maybe
 
-interface CacheInterface<Key, Value> {
+interface CacheInterface<K, V> {
 
-    fun put(value: Value)
-    fun putAll(values: List<Value>)
+    fun put(value: V)
+    fun putAll(values: List<V>)
     fun clear()
-    fun getAll(): Maybe<List<Value>>
-    fun get(key: Key): Maybe<Value>
+    fun getAll(): Maybe<List<V>>
+    fun get(key: K): Maybe<V>
 }
